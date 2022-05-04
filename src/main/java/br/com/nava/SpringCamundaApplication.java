b14b526a756291f0b049d789819dc5bfb0ac53b2
Karine Tavares <karinetavaresds12@gmail.com>
2022-05-04 12:22:43 -0300

    my first commit

---
+++ src/main/java/br/com/nava/SpringCamundaApplication.java
@@ -0,0 +1,13 @@
+package br.com.nava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCamundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCamundaApplication.class, args);
+	}
+
+}
